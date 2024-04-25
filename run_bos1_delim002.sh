39dd818b16426ef1072aeaba0b96e1fc0549416d
Tianyu SLURM <tianyu_guo@berkeley.edu>
2024-04-25 07:02:16 +0000

    update data

--- run_bos1_delim002.sh
+++ run_bos1_delim002.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-user=tianyu_guo@berkeley.edu
 #SBATCH --cpus-per-task=16
 #SBATCH --gres=gpu:1
-#SBATCH --array=0
+#SBATCH --array=0-8
 conda init bash
 source activate nanogpt
 
@@ -19,7 +19,7 @@ python ihead_full_main.py max_iters=5000 log_probes=True eval_delta=5  model_arg
         data_args.k=5 data_args.fixed_special_toks=True data_args.bos_num=1\
         data_args.delimiter_p=0.02\
         optim_args.use_sgd=False optim_args.learning_rate=0.0003 optim_args.weight_decay=1e-4 optim_args.batch_size=512 \
-        model_args.dim=256 save_dir=/data/tianyu_guo/birth/gens/model_L${layer}_H${head}_lr3-4_bos1_delim002
+        model_args.dim=256 save_dir=/data/tianyu_guo/birth/gens/delim/model_L${layer}_H${head}_lr3-4_bos1_delim002
 
 # python ihead_full_main.py max_iters=5000 log_probes=True eval_delta=5  model_args.n_layers=3 model_args.n_heads=2 \
 #         data_args.k=5 data_args.fixed_special_toks=True data_args.bos_num=0\
